023fca6f91499150ac52c83ea42a87f092ad6c56
sachin.webonise <sachin.wagh@weboniselab.com>
2014-01-06 21:52:01 +0530

    Gem working

--- lib/shop_inventory.rb
+++ lib/shop_inventory.rb
@@ -2,14 +2,12 @@ require_relative "shop_inventory/shop"
 require_relative "shop_inventory/shopkeeper"
 require_relative "shop_inventory/product"
 module ShopInventory
-=begin 
+
   class ShopInventory
     def self.start
-      
+      shop=Shop.new
+      shop.go_to_shop
     end
-
-  end
-=end
-  shop=Shop.new
-  shop.go_to_shop
+  end  
+  
 end
